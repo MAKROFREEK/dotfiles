6f0f578539d29544aa880c49e5f0ae2ccdd92e68
MAKROFREEK <84992148+MAKROFREEK@users.noreply.github.com>
2023-09-26 21:01:21 -0500

    Delete get-packages-2.sh

--- get-packages-2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-
-yay -Sy kitty neovim git curl ocs-url gnome-tweaks zzz slock-git neovim neofetch fish fisher omf intellij code ttf-ubuntu-mono-nerd nvim-lazy gradience papirus-folders-git adw-gtk3
-
-papirus-folders -C white
