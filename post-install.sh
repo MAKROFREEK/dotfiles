d8919512417a0504013c55aa157dad22acc79363
MAKROFREEK <84992148+MAKROFREEK@users.noreply.github.com>
2024-12-23 11:21:49 -0600

    make packages-list functional

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e  # Exit on any error
+# set -e  # Exit on any error
 
 # Copy folders
 cp -r dotfiles/.config/* ~/.config
@@ -9,13 +9,34 @@ cp -r dotfiles/.local/* ~/.local
 mkdir -p ~/.themes
 cp -r dotfiles/.themes/* ~/.themes
 
-# Update and upgrade the system
-echo "Updating system..."
-sudo apt update && sudo apt upgrade -y
+# Neovim 
+# VimPlug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Ensure latest version
+sudo add-apt-repository ppa:neovim-ppa/stable
+
+# Check if the packages-list file exists
+if [ ! -f "packages-list" ]; then
+    echo "Error: packages-list file not found!"
+    exit 1
+fi
+
+# Read the packages from the file and install them
+echo "Installing packages from packages-list..."
+while IFS= read -r package; do
+    # Ignore empty lines and comments (lines starting with #)
+    if [[ -n "$package" && ! "$package" =~ ^# ]]; then
+        echo "Installing $package..."
+        sudo apt install -y "$package"
+    fi
+done < "packages-list"
 
 # Install packages
 # echo "Installing packages..."
 # sudo apt install -y software-properties-common curl wget gnupg kitty neovim neofetch git btop dunst ranger zathura zathura-pdf-poppler rofi vlc steam spotify-client gnome-tweaks
+
+
 
 # Final system update
 echo "Updating and cleaning up..."
@@ -23,10 +44,6 @@ sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 echo "All packages have been installed successfully!"
 
-# Neovim Vim Plug
-# Will still need to run :PlugInstall in neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 # Download and copy backgrounds
 bash dotfiles/get-backgrounds.sh
+
